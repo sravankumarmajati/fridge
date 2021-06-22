96fda5794aef2a46a747f2242fab148cc82adff0
sravan <kumar.sravan561>
2021-06-22 20:11:05 +0530

    adding files

---
+++ src/test/java/flipkart/MyTest.java
@@ -0,0 +1,17 @@
+package flipkart;
+
+import org.junit.Test;
+
+import br.gov.frameworkdemoiselle.behave.controller.BehaveContext;
+
+public class MyTest {
+
+	private BehaveContext eng = BehaveContext.getInstance();
+
+	@Test
+	public void testAllObras() throws Throwable {
+		eng.addStories("/stories");
+		eng.run();
+	}
+
+}
